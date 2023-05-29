c6e6714b085f8adb3649f4a71a2c00646fd803eb
miusarname <oscarm.alvarezg@gmail.com>
2023-05-29 17:06:17 -0500

    feat: :sparkles: Definiciones de constantes
    .

--- index.php
+++ index.php
@@ -10,7 +10,30 @@ $texto="MUndo";
 $mensaje = sprintf("Hola %s",$texto); // Basicamente retorna a la variable  "Hola MUndo"
 echo $mensaje;  //* Esto imprimira <code> Hola MUndo </code>
 
+//? Variables y constantes
 
+// Declarar una variable númerica 
+$edad = 25;
 
+// Declarar una variable de texto 
+$nombre = "JUan";
 
+// Declarar una variable booleana
+$es_valido =  true;
+echo  var_dump($nombre);
 
+//! Definicion de constante a la "antigua"
+
+// Declarar constante numerica
+define("Pi",3.1416180);
+
+// Declarar una constante de texto 
+define("SALUDO","Hola mundo");
+
+//Declarar una constante boolean
+define("ES_VALIDO",true);
+
+//o "nueva" forma
+
+const texto = "TEzto";
+    
