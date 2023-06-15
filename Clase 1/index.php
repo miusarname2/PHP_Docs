d2ad0b564f3193c8bb6372435aae8a48c538c180
miusarname <oscarm.alvarezg@gmail.com>
2023-06-15 14:39:20 -0500

    feat: :sparkles: Se añaden la documentacion del 14/06/2023

--- Clase 1/index.php
+++ Clase 1/index.php
@@ -1286,4 +1286,328 @@ class Perro extends Animal {
 $perro = new Perro("Max");
 $perro->comer(); // Output: Max está comiendo.
 $perro->ladrar(); // Output: Max está ladrando.
-?>
+
+
+//14/06/2023
+
+// Este código define dos interfaces, Saludar y Saludar2.
+// Una interfaz es un contrato que define un conjunto de métodos que debe implementar una clase.
+interface Saludar {
+    public function getNumero();
+    public function setNumero($numero);
+    public function getNome();
+}
+
+interface Saludar2 {
+    public function getNumeroRamdom();
+    public function setNumeroRamdom($numero);
+}
+
+// Este código define una clase llamada Hola2 que extiende la clase Hola e implementa las interfaces Saludar y Saludar2.
+class Hola2 extends Hola implements Saludar, Saludar2 {
+
+// Este constructor inicializa el objeto Hola2 con el número y nombre dados.
+    function __construct($numero,$nome){
+        parent::__construct($numero,$nome);
+    }
+
+    // Este método imprime el número 1.
+    public function getNumero(){
+        print_r(1);
+    }
+
+    // Este método no hace nada.
+    public function setNumero($numero){}
+
+    // Este método no hace nada.
+    public function getNome(){}
+
+    // Este método no hace nada.
+    public function getNumeroRamdom(){}
+
+    // Este método no hace nada.
+    public function setNumeroRamdom($numero){}
+
+}
+
+
+
+/*
+*Se define una clase abstracta llamada Animal 
+*utilizando la palabra clave abstract. 
+?Una clase abstracta no se puede instanciar directamente, 
+?se utiliza como una clase base para otras clases derivadas. 
+*/
+abstract class Animal {
+    /*
+    *La clase Animal tiene una propiedad protegida $nombre y un método abstracto sonidoAnimal().
+    */
+    protected $nombre;
+    protected $apellido;
+    protected $edad;
+    /*
+    *La clase abstracta Animal tiene un constructor llamado __construct() que recibe un parámetro $nombre
+    *y lo asigna a la propiedad protegida $nombre.
+    */
+    public function __construct($nombre,$apellido,$edad) {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->setEdad($edad); // Utilizamos el método setEdad para validar el valor
+    }
+    /*
+    *El método sonidoAnimal() es declarado como ABSTRACTO utilizando la palabra clave abstract.
+    ?Esto significa que cualquier CLASE derivada de Animal debe implementar este método.
+    */
+    abstract public function sonidoAnimal();
+    
+    /*
+    *Agregar un método público para obtener el valor de la propiedad protegida $nombre.
+    */
+    public function getNombre() {
+        return $this->nombre;
+    }
+    public function getApellido() {
+        return $this->apellido;
+    }
+    public function getEdad() {
+        return $this->edad;
+    }
+    public function setEdad($edad) {
+        if (is_numeric($edad) && $edad > 0) {
+            $this->edad = $edad;
+        } else {
+            // Si el valor no es un número válido, asignamos un valor predeterminado
+            $this->edad = 1;
+        }
+    }
+}
+
+/*
+*Se definen dos clases derivadas de Animal: Perro y Gato. 
+?Ambas clases implementan el método sonidoAnimal() que se define en la clase abstracta Animal.
+*/
+class Perro extends Animal {
+    public function sonidoAnimal() {
+        return "Guau Guau Guau!";
+    }
+}
+
+class Gato extends Animal {
+    public function sonidoAnimal() {
+        return "Miau Miau Miau!";
+    }
+}
+
+/*
+*Se crean objetos de las clases derivadas: $perro de la clase Perro y $gato de la clase Gato. 
+*Se proporciona un nombre en el constructor de cada objeto.
+*/
+// Crear objetos de las clases derivadas
+$perro = new Perro("Sheila","Cubides",32);
+$gato = new Gato("Angel","Ramirez",20);
+
+/*
+*Se llama al método sonidoAnimal() en cada objeto ($perro->sonidoAnimal() y $gato->sonidoAnimal()) y se imprime el resultado utilizando echo. 
+*El método sonidoAnimal() devuelve el sonido característico de cada animal.
+*/
+
+// Llamar al método sonidoAnimal() y getNombre()
+echo "El perro: " . $perro->getNombre() . " " . $perro->getApellido() . " tiene una edad de " . $perro->getEdad() . " y hace el sonido:  " . $perro->sonidoAnimal() . "<br>";
+echo "El gato: " . $gato->getNombre() . " " . $gato->getApellido() . " tiene una edad de " . $gato->getEdad() . " y hace el sonido:  " . $gato->sonidoAnimal() . "<br>";
+
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                   traits                                   */
+/* -------------------------------------------------------------------------- */
+
+/**
+ *  no es posible invocar directamente parent::
+ *  dentro de una clase que utiliza un trait para acceder 
+ *  a los métodos o propiedades de la clase padre.
+ * 
+ * no siguen la misma relación de herencia.
+ * Los traits son mecanismos de reutilización de código que se pueden incluir en múltiples clases. 
+ * No hay una jerarquía de clases con traits.
+ * 
+*/
+
+trait MiTrait
+{
+    public function miMetodo()
+    {
+        return "¡Hola desde el trait!";
+    }
+}
+class MiClase
+{
+    use MiTrait;
+    public function llamarMetodoTrait()
+    {
+        return $this->miMetodo();
+    }
+}
+
+$objeto = new MiClase();
+echo $objeto->llamarMetodoTrait(); 
+
+/* -------------------------------------------------------------------------- */
+/*                                  SINGLETON                                 */
+/* -------------------------------------------------------------------------- */
+echo "<h3>traits</h3>";
+
+trait Singleton
+{
+    private int $atributoishon = 12;
+    private static $instance;
+    public static function getInstance()
+    {
+        if (!(self::$instance instanceof self)) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+    public function getAtributoishon()
+    {
+        return $this->atributoishon;
+    }
+
+    public function setVariableishon($atributoishon)
+    {
+        $this->atributoishon = $atributoishon;
+    }
+}
+
+class DbReaderT
+{
+    use Singleton;
+}
+
+class FileReaderT
+{
+    use Singleton;
+}
+
+$a = DbReaderT::getInstance();
+$b = DbReaderT::getInstance();
+/**
+ * se modifica en ambas instancias, porque ambas isntancias apuntan
+ * a la misma referencia de memoria
+ */
+$b->setVariableishon(3);
+var_dump($a);
+var_dump($b);
+
+/* -------------------------------------------------------------------------- */
+/*                              multiples traits                              */
+/* -------------------------------------------------------------------------- */
+
+
+trait Saludar
+{
+    function decirHola()
+    {
+        return "hola";
+    }
+}
+trait Despedir
+{
+    function decirAdios()
+    {
+        return "adios";
+    }
+}
+class Comunicacion
+{
+    use Saludar, Despedir;
+}
+$comunicacion = new Comunicacion();
+echo $comunicacion->decirHola() . ", que tal. " . $comunicacion->decirAdios();
+
+echo "<br>";
+
+/* -------------------------------------------------------------------------- */
+/*                           traits dentro de traits                          */
+/* -------------------------------------------------------------------------- */
+
+trait SaludoYDespedida
+{
+    use Saludar, Despedir;
+}
+
+class ComunicacionM
+{
+    use SaludoYDespedida;
+}
+$comunicacion = new ComunicacionM;
+echo $comunicacion->decirHola() . ", que tal. " . $comunicacion->decirAdios();
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                          conflictos entre metodos                          */
+/* -------------------------------------------------------------------------- */
+
+// ----- 1 
+
+trait JuegoE
+{
+    function play()
+    {
+        echo "Jugando a un juego";
+    }
+}
+trait MusicaE
+{
+    function play()
+    {
+        echo "Escuchando música";
+    }
+}
+class ReproductorE
+{
+    use JuegoE, MusicaE;
+}
+$reproductor = new ReproductorE;
+$reproductor->play();
+// Devuelve Fatal error: Trait method play has not been applied,
+// because there are collisions with other trait methods on Reproductor
+
+
+// ---- 2
+
+trait Saludare
+{
+    public function decirHola()
+    {
+        return "Hola desde el trait";
+    }
+}
+class MiClasee
+{
+    use Saludare;
+    public function decirHola()
+    {
+        return "Hola desde la clase";
+    }
+}
+$objeto = new MiClasee();
+echo $objeto->decirHola();
+
+// ----- 3
+
+class ReproductorAS
+{
+    use JuegoE, MusicaE {
+        JuegoE::play as playDeJuego;
+        MusicaE::play insteadof JuegoE;
+    }
+}
+
+$reproductor = new ReproductorAS;
+$reproductor->play(); // Devuelve: Escuchando música
+$reproductor->playDeJuego(); // Devuelve: Jugando a un juego
+
+
