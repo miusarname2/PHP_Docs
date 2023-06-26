233382b16c777c36fb7d9a22159b5b97ad7292e5
miusarname <oscarm.alvarezg@gmail.com>
2023-06-26 12:37:41 -0500

    feat: :sparkles: Se completo el primer CRUD con Bases de Datos

--- conexionPHP/index.php
+++ conexionPHP/index.php
@@ -17,22 +17,47 @@ echo json_encode($res);
 });
 
 $router->delete('/delete', function() {
-
+    try{
+    $connect = new \Apps\connect();
+    $data= json_decode(file_get_contents('php://input'),true);
+    $sql = "DELETE FROM tb_camper WHERE id= :id";
+    $stmt = $connect->con->prepare($sql);
+    $stmt->bindParam(':id', $data['id']);
+    $stmt->execute();
+    }catch(\PDOException $e){
+        print_r($e->getMessage());
+    }
 });
 
 $router->post('/post', function() {
+    try{
+        $connect = new \Apps\connect();
+        $data= json_decode(file_get_contents('php://input'),true);
+        $sql = "INSERT INTO tb_camper (nombre, edad) VALUES (:nombre, :edad)";
+        $stmt = $connect->con->prepare($sql);
+        $stmt->bindParam(':nombre', $data['nombre']);
+        $stmt->bindParam(':edad', $data['edad']);
+        $stmt->execute();
+        print_r("Inserted");
+    
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+});
+
+$router->put('/put', function() {
+    try{
     $connect = new \Apps\connect();
-    $data = [
-        'name' => 'John Doe',
-        'email' => 'johndoe@example.com',
-        'password' => '123456',
-    ];
-    $sql = "INSERT INTO tb_camper (name, email, password) VALUES (:name, :email, :password)";
-    $stmt = $connect->with->prepare($sql);
-    $stmt->bindParam(':name', $data['name']);
-    $stmt->bindParam(':email', $data['email']);
-    $stmt->bindParam(':password', $data['password']);
+    $data= json_decode(file_get_contents('php://input'),true);
+    $sql = "UPDATE tb_camper SET nombre=:nombre, edad=:edad WHERE id=:id";
+    $stmt = $connect->con->prepare($sql);
+    $stmt->bindParam(':id', $data['id']);
+    $stmt->bindParam(':nombre', $data['nombre']);
+    $stmt->bindParam(':edad', $data['edad']);
     $stmt->execute();
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
 });
 
 // Run it!
